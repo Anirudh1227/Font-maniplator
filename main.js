a194ad6b4a5fad327a9d69b6041e51ac9b75158a
Anirudh1227 <84142649+Anirudh1227@users.noreply.github.com>
2022-10-17 21:03:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0;
+diffrence = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video =createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas (550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modeLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modeLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+function draw() {
+background('#969A97');
+
+    document.getElementById("text_size").innerHTML = "size of the text will be = " + diffrence +"px"
+    fill('#F90093');
+    stroke('#F90093');
+    textSize(diffrence);
+    text("fhirghidshfihdfihijfjghijfsgigfiofs",noseX, noseY);   
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0 )
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console,log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        diffrence = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = "+ rightWristX + "differece = " + diffrence)
+    }
+}
